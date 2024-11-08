badb6d09f4ff48f0c49db504a50f14a0560e790e
Zncl2222 <3002shinning@gmail.com>
2024-11-08 22:54:12 +0800

    feat: add ReLU and Sigmoid

--- network/activation.py
+++ network/activation.py
@@ -8,5 +8,24 @@ class Tanh(BaseLayer):
         self.input = input
         return np.tanh(input)
 
-    def backward(self, output_error, learning_rate=0.01):
+    def backward(self, output_error: np.ndarray, learning_rate: float = 0.01):
         return (1 - np.tanh(self.input) ** 2) * output_error
+
+
+class ReLU(BaseLayer):
+    def forward(self, input: np.ndarray):
+        self.input = input
+        return np.maximum(0, self.input)
+
+    def backward(self, output_error: np.ndarray, learning_rate: float = 0.01):
+        return output_error * (self.input > 0)
+
+
+class Sigmoid(BaseLayer):
+    def forward(self, input: np.ndarray):
+        self.input = input
+        return 1 / (1 + np.exp(-input))
+
+    def backward(self, output_error: np.ndarray, learning_rate: float = 0.01):
+        sigmoid_output = self.forward(self.input)
+        return output_error * sigmoid_output * (1 - sigmoid_output)
